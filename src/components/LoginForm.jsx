ed0f36d6e19a77726d0fd2f15a3d29aa137dbd4e
Gaby <matiascoria427@gmail.com>
2024-01-28 15:04:34 -0300

    inicio de la configuracion de react router

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,15 @@
+export default function LoginForm() {
+  return (
+    <div>
+        <form>
+          <label htmlFor="">Usuario</label>
+            <input type="text" />
+
+            <label htmlFor="">Password</label>
+            <input type="password" />
+
+            <button>INGRESAR</button>
+        </form>
+    </div>
+  )
+}
